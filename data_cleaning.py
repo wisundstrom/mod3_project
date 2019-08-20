494b8f397fe027babdcacc7344b995d214862c8e
Alison <31246169+aapeebles@users.noreply.github.com>
2019-08-20 16:33:22 -0400

    update files for project

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,7 +3,7 @@ This module is for your data cleaning.
 It should be repeatable.
 
 ## PRECLEANING
-There should be a seperate script recording how you transformed the json api calls into a dataframe and csv.
+There should be a separate script recording how you transformed the json api calls into a dataframe and csv.
 
 ## SUPPORT FUNCTIONS
 There can be an unlimited amount of support functions.
@@ -11,24 +11,27 @@ Each support function should have an informative name and return the partially c
 """
 import pandas as pd
 
-def support_function_one():
+def support_function_one(example):
     pass
 
-def support_function_two():
+def support_function_two(example):
     pass
 
-def support_function_three():
+def support_function_three(example):
     pass
 
-
-def full_clean(dirty_dala):
+def full_clean():
     """
     This is the one function called that will run all the support functions.
-    :param dirty_dala: should be a saved csv
+    Assumption: Your data will be saved in a data folder and named "dirty_data.csv"
+
     :return: cleaned dataset to be passed to hypothesis testing and visualization modules.
     """
-    cleaning_data = support_function_one(dirty_data)
+    dirty_data = pd.read_csv("./data/dirty_data.csv")
+
+    cleaning_data1 = support_function_one(dirty_data)
     cleaning_data2 = support_function_two(cleaning_data1)
     cleaned_data= support_function_three(cleaning_data2)
     cleaned_data.to_csv('./data/cleaned_for_testing.csv')
+    
     return cleaned_data
